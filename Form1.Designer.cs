8a2361b470890fbfcf40b2e9d1b08122e9decc6c
KhDmitr96 <KhDmitr96@yandex.ru>
2021-12-13 14:04:21 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace CRB_winForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.receiveDateLbl = new System.Windows.Forms.Label();
+            this.receiveDateTb = new System.Windows.Forms.TextBox();
+            this.receiveDataGridView = new System.Windows.Forms.DataGridView();
+            this.receiveBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.receiveDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // receiveDateLbl
+            // 
+            this.receiveDateLbl.AutoSize = true;
+            this.receiveDateLbl.Location = new System.Drawing.Point(12, 15);
+            this.receiveDateLbl.Name = "receiveDateLbl";
+            this.receiveDateLbl.Size = new System.Drawing.Size(119, 13);
+            this.receiveDateLbl.TabIndex = 0;
+            this.receiveDateLbl.Text = "Запрашиваемая дата:";
+            // 
+            // receiveDateTb
+            // 
+            this.receiveDateTb.Location = new System.Drawing.Point(137, 12);
+            this.receiveDateTb.MaxLength = 10;
+            this.receiveDateTb.Name = "receiveDateTb";
+            this.receiveDateTb.Size = new System.Drawing.Size(100, 20);
+            this.receiveDateTb.TabIndex = 1;
+            // 
+            // receiveDataGridView
+            // 
+            this.receiveDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.receiveDataGridView.Location = new System.Drawing.Point(12, 38);
+            this.receiveDataGridView.Name = "receiveDataGridView";
+            this.receiveDataGridView.Size = new System.Drawing.Size(419, 400);
+            this.receiveDataGridView.TabIndex = 3;
+            // 
+            // receiveBtn
+            // 
+            this.receiveBtn.Location = new System.Drawing.Point(243, 10);
+            this.receiveBtn.Name = "receiveBtn";
+            this.receiveBtn.Size = new System.Drawing.Size(75, 23);
+            this.receiveBtn.TabIndex = 2;
+            this.receiveBtn.Text = "Получить";
+            this.receiveBtn.UseVisualStyleBackColor = true;
+            this.receiveBtn.Click += new System.EventHandler(this.receiveBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 450);
+            this.Controls.Add(this.receiveBtn);
+            this.Controls.Add(this.receiveDataGridView);
+            this.Controls.Add(this.receiveDateTb);
+            this.Controls.Add(this.receiveDateLbl);
+            this.Name = "Form1";
+            this.Text = "Получение курса валют на выбранную дату";
+            ((System.ComponentModel.ISupportInitialize)(this.receiveDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label receiveDateLbl;
+        private System.Windows.Forms.TextBox receiveDateTb;
+        private System.Windows.Forms.Button receiveBtn;
+        private System.Windows.Forms.DataGridView receiveDataGridView;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using CRB_winForms.cbrf;
+
+namespace CRB_winForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            receiveDateTb.Text = DateTime.Today.ToString("dd/MM/yyyy");
+            receiveDataGridView.Columns.Add("vname", "Сurrency Name");
+            receiveDataGridView.Columns.Add("vcurs", "Exchange Rate");
+            receiveDataGridView.Columns["vname"].DataPropertyName = "vname";
+            receiveDataGridView.Columns["vname"].Width = 150;
+            receiveDataGridView.Columns["vcurs"].DataPropertyName = "vcurs";
+            receiveDataGridView.Columns["vcurs"].DefaultCellStyle.Format = "c";
+        }
+
+        private void receiveBtn_Click(object sender, EventArgs e)
+        {
+            System.DateTime receiveDate = Convert.ToDateTime(receiveDateTb.Text);
+            DailyInfo di = new DailyInfo();
+            DataSet curs = di.GetCursOnDate(receiveDate);
+
+            receiveDataGridView.DataSource = curs.Tables[0];
+            foreach (DataGridViewColumn column in receiveDataGridView.Columns)
+                if (column.Name != "vname" && column.Name != "vcurs")
+                    column.Visible = false;
+        }
+    }
+}
